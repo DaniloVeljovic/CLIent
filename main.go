f47eafac2758cbe073c78b175e401686430d19a9
Danilo Veljovic <danilo.veljovic@zalando.de>
2024-06-18 13:40:30 +0200

    Selectively apply

--- main.go
+++ main.go
@@ -5,15 +5,53 @@ import (
 	"github.com/rivo/tview"
 )
 
+type Collection struct {
+	Name string
+}
+
+type Request struct {
+	Name string
+}
+
+func getCollections() []Collection {
+	return []Collection{
+		{Name: "Collection 1"},
+		{Name: "Collection 2"},
+	}
+}
+
+func getRequests(collection *Collection) Request {
+	m := make(map[string]Request)
+	m["Collection 1"] = Request{Name: "Request 1"}
+	m["Collection 2"] = Request{Name: "Request 2"}
+	return m[collection.Name]
+}
+
 func main() {
 	app := tview.NewApplication()
 
+	collections := getCollections()
+
 	// Define UI components
-	collectionsPanel := tview.NewList().AddItem("List Item 1", "", '0', nil).AddItem("List Item 1", "ddd", 'q', nil)
+	collectionsPanel := tview.NewList()
+
 	collectionsPanel.SetBorder(true).SetTitle("Collections (c)")
-	requestsPanel := tview.NewBox().SetBorder(true).SetTitle("Requests")
+	requestsPanel := tview.NewList()
+	requestsPanel.SetBorder(true).SetTitle("Requests")
 	requestEditor := tview.NewBox().SetBorder(true).SetTitle("Request Editor")
 	responseViewer := tview.NewBox().SetBorder(true).SetTitle("Response")
+
+	for _, collection := range collections {
+		collectionsPanel.AddItem(collection.Name, "", 0, func() {
+			r := getRequests(&collection)
+
+			for i := range requestsPanel.GetItemCount() {
+				requestsPanel.RemoveItem(i)
+			}
+
+			requestsPanel.AddItem(r.Name, "", 0, nil)
+		})
+	}
 
 	// Layout the panels
 	leftPanel := tview.NewFlex().SetDirection(tview.FlexRow).
