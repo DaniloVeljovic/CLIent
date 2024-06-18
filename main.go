d218a0318581cfb9b6d11238c7f99ad90b108620
Danilo Veljovic <danilo.veljovic@zalando.de>
2024-06-18 21:37:57 +0200

    Selectively apply

--- main.go
+++ main.go
@@ -1,89 +1,106 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"io"
 	"net/http"
-	_ "net/http"
+	"os"
 	"strings"
-	_ "strings"
-)
-
-type Verb string
-
-const (
-	GET    Verb = "GET"
-	POST   Verb = "POST"
-	PUT    Verb = "PUT"
-	PATCH  Verb = "PATCH"
-	DELETE Verb = "DELETE"
 )
 
 type Collection struct {
-	Name string
+	Id       int
+	Name     string
+	Requests []Request
 }
 
 type Request struct {
+	Id      int
 	Name    string
-	Headers map[string]string
+	Verb    string
 	Url     string
-	Verb    Verb
-	Body    string
+	Headers map[string]string
+	Body    interface{}
 }
 
 func getCollections() []Collection {
-	return []Collection{
-		{Name: "Collection 1"},
-		{Name: "Collection 2"},
+	file, err := os.ReadFile("./db/collection.json")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil
 	}
+
+	var collections []Collection
+
+	err = json.Unmarshal(file, &collections)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return nil
+	}
+
+	return collections
 }
 
-func getRequests(collection *Collection) []Request {
-	m := make(map[string][]Request)
-	m["Collection 1"] = []Request{
-		{Name: "Request 1"},
-		{Name: "Request 2"},
+func saveCollections(collections []Collection) {
+	file, err := os.Create("./db/collection.json")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
 	}
-	m["Collection 2"] = []Request{
-		{Name: "Request 3", Verb: GET, Url: "https://www.google.com"},
-		{Name: "Request 4", Verb: GET, Url: "https://www.postman-echo.com/get"},
+	defer file.Close()
+
+	data, err := json.MarshalIndent(collections, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		return
 	}
-	return m[collection.Name]
+
+	_, err = file.Write(data)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
 }
 
 func main() {
 	app := tview.NewApplication()
-
 	collections := getCollections()
-
-	// Define UI components
 	collectionsPanel := tview.NewList()
-
 	collectionsPanel.SetBorder(true).SetTitle("Collections (c)")
 	requestsPanel := tview.NewList()
 	requestsPanel.SetBorder(true).SetTitle("Requests (r)")
 	requestEditor := tview.NewTextArea()
 	requestEditor.SetBorder(true).SetTitle("Request Editor (e)")
 	responseViewer := tview.NewTextView()
-	responseViewer.SetBorder(true).SetTitle("Response")
+	responseViewer.SetBorder(true).SetTitle("Response (v)")
 
-	for _, collection := range collections {
+	var activeRequest *Request
+	//var activeCollection *Collection
+
+	for i := range collections {
+		collection := &collections[i]
 		collectionsPanel.AddItem(collection.Name, "", 0, func() {
-			r := getRequests(&collection)
+			requestsPanel.Clear()
 
-			for i := range requestsPanel.GetItemCount() {
-				requestsPanel.RemoveItem(i)
-			}
-
-			for _, r := range r {
-				requestsPanel.AddItem(r.Name, "", 0, func() {
+			for j := range collection.Requests {
+				//activeCollection = &collections[j]
+				request := &collection.Requests[j]
+				requestsPanel.AddItem(request.Name, "", 0, func() {
+					activeRequest = request
 					builder := strings.Builder{}
-					builder.WriteString(string(r.Verb))
-					builder.WriteString(" ")
-					builder.WriteString(r.Url)
-
-					requestEditor.SetText(builder.String(), false)
+					builder.WriteString(request.Verb)
+					builder.WriteString("\n")
+					builder.WriteString(request.Url)
+					builder.WriteString("\n")
+					headers, _ := json.Marshal(request.Headers)
+					builder.WriteString(string(headers))
+					builder.WriteString("\n")
+					body, _ := json.Marshal(request.Body)
+					builder.WriteString(string(body))
+					requestEditor.SetText(builder.String(), true)
 				})
 			}
 			app.SetFocus(requestsPanel)
@@ -100,36 +117,87 @@ func main() {
 		AddItem(responseViewer, 0, 2, false)
 
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyRune {
-			switch event.Rune() {
-			case 'c':
-				app.SetFocus(collectionsPanel)
-				return nil
-			case 'q':
-				app.Stop()
-				return nil
-			case 'r':
-				app.SetFocus(requestsPanel)
-				return nil
-			case 'e':
-				app.SetFocus(requestEditor)
-				return nil
-			case 'p':
-				get, err := http.Get("https://www.postman-echo.com/get")
-				if err != nil {
+		if event.Key() == tcell.KeyEsc {
+			if activeRequest != nil {
+				text := requestEditor.GetText()
+				lines := strings.SplitN(text, "\n", 4)
+				if len(lines) == 4 {
+					activeRequest.Verb = lines[0]
+					activeRequest.Url = lines[1]
+					json.Unmarshal([]byte(lines[2]), &activeRequest.Headers)
+					json.Unmarshal([]byte(lines[3]), &activeRequest.Body)
+					saveCollections(collections)
+				}
+			}
+			app.SetFocus(mainPanel)
+		} else if event.Key() == tcell.KeyRune {
+			if app.GetFocus() != requestEditor {
+				switch event.Rune() {
+				case 'c':
+					app.SetFocus(collectionsPanel)
+					return nil
+				case 'q':
+					app.Stop()
+					return nil
+				case 'r':
+					app.SetFocus(requestsPanel)
+					return nil
+				case 'e':
+					app.SetFocus(requestEditor)
+					return nil
+				case 'p':
+					response := CallApi(requestEditor.GetText())
+					responseViewer.SetText(response)
+					return nil
+				case 'v':
+					app.SetFocus(responseViewer)
 					return nil
 				}
-				defer get.Body.Close()
-				body, _ := io.ReadAll(get.Body)
-				responseViewer.SetText(string(body))
-				return nil
 			}
-
 		}
+
 		return event
 	})
 
 	if err := app.SetRoot(mainPanel, true).SetFocus(mainPanel).Run(); err != nil {
 		panic(err)
 	}
+}
+
+func CallApi(text string) string {
+	client := &http.Client{}
+	params := strings.SplitN(text, "\n", 4)
+	if len(params) < 4 {
+		return "Invalid input format. Expected: <METHOD> <URL> <HEADERS_JSON> <BODY>"
+	}
+
+	method, url, headersJson, bodyJson := params[0], params[1], params[2], params[3]
+
+	req, err := http.NewRequest(method, url, bytes.NewBufferString(bodyJson))
+	if err != nil {
+		return "Failed to create request: " + err.Error()
+	}
+
+	var headers map[string]string
+	err = json.Unmarshal([]byte(headersJson), &headers)
+	if err != nil {
+		return "Failed to parse headers JSON: " + err.Error()
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "An error occurred: " + err.Error()
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "Failed to read response body: " + err.Error()
+	}
+
+	return string(body)
 }
